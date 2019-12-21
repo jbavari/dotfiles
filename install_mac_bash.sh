2efda272fe1a97186c40c9333dbe8afd925f06b5
Josh Bavari <joshbavari@Joshs-MacBook-Pro.local>
2019-12-21 10:57:48 -0700

    Add in automation to help set up a new mac

---
+++ install_mac_bash.sh
@@ -0,0 +1,4 @@
+# install the packages for sublime text 3 on mac
+SUBLIME_PACKAGE_PATH="/Users/$USER/Library/Application Support/Sublime Text 3/Packages/User"
+mkdir $SUBLIME_PACKAGE_PATH
+cp SublimePackages.json "${SUBLIME_PACKAGE_PATH}/Package Control.sublime-settings"
